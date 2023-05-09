8a93aab188612beeef00f9a67e8482f6dd5328c5
German Sanchez <germands2013>
2023-05-09 17:14:41 -0300

    little fix when displaying cards

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -6,50 +6,53 @@ let totalCarrito = document.getElementById("total");
 let sumaTotal = 0;
 //=========== USO DE API ===========
 const adminLogin = JSON.parse(localStorage.getItem("adminLogin")) || {
-    admin: false
-}
+  admin: false,
+};
 
 // ========= CONDICIONAL SI ES ADMIN ==========
 if (adminLogin.admin) {
-    navLogin.innerText = "Cerrar Sesión";
-    insertUser.innerHTML = `
+  navLogin.innerText = "Cerrar Sesión";
+  insertUser.innerHTML = `
                 <p id="user-name">Hola, Administrador</p>
                 <img src="../assets/img/default-user-img.png" id="user-pic">
-                `
+                `;
 } else {
-    const crearUsuario = () => {
-        let nombreUser = JSON.parse(sessionStorage.getItem("nombreUser"));
-        let apellidoUser = JSON.parse(sessionStorage.getItem("apellidoUser"));
-        let fotoUser = JSON.parse(sessionStorage.getItem("fotoUser"));
-        insertUser.innerHTML = `
+  const crearUsuario = () => {
+    let nombreUser = JSON.parse(sessionStorage.getItem("nombreUser"));
+    let apellidoUser = JSON.parse(sessionStorage.getItem("apellidoUser"));
+    let fotoUser = JSON.parse(sessionStorage.getItem("fotoUser"));
+    insertUser.innerHTML = `
                 <p id="user-name">Hola, ${nombreUser} ${apellidoUser}</p>
                 <img src="${fotoUser}" id="user-pic">
-                `
-    }
-    if (sessionStorage.getItem("nombreUser") != null) {
-        crearUsuario();
-    } else {
-        fetch('https://randomuser.me/api/')
-            .then(response => response.json())
-            .then(resultado => {
-                // manda perfil a sessionStorage
-                let apiNombre = resultado.results[0].name.first;
-                let apiApellido = resultado.results[0].name.last;
-                let apiFoto = resultado.results[0].picture.medium;
-                sessionStorage.setItem("nombreUser", JSON.stringify(apiNombre));
-                sessionStorage.setItem("apellidoUser", JSON.stringify(apiApellido));
-                sessionStorage.setItem("fotoUser", JSON.stringify(apiFoto));
-                // generar html con API
-                insertUser.innerHTML = `
+                `;
+  };
+  if (sessionStorage.getItem("nombreUser") != null) {
+    crearUsuario();
+  } else {
+    fetch("https://randomuser.me/api/")
+      // .then((response) => response.json())
+      .then((resultado) => {
+        // manda perfil a sessionStorage
+        let apiNombre = resultado.results[0].name.first;
+        let apiApellido = resultado.results[0].name.last;
+        let apiFoto = resultado.results[0].picture.medium;
+        sessionStorage.setItem("nombreUser", JSON.stringify(apiNombre));
+        sessionStorage.setItem("apellidoUser", JSON.stringify(apiApellido));
+        sessionStorage.setItem("fotoUser", JSON.stringify(apiFoto));
+        // generar html con API
+        insertUser.innerHTML = `
                 <p id="user-name">Hola, ${apiNombre} ${apiApellido}</p>
                 <img src="${apiFoto}" id="user-pic">
-                `
-            })
-            .catch(error => console.log(error), insertUser.innerHTML = `
+                `;
+      })
+      .catch(
+        (error) => console.log(error),
+        (insertUser.innerHTML = `
             <p id="user-name">Cargando usuario...</p>
             <img src="./assets/img/default-user-img.png" id="user-pic">
             `)
-    }
+      );
+  }
 }
 
 // ======== NUMERO CARRITO EN NAVBAR ===========
@@ -57,106 +60,106 @@ let prodComprados = document.getElementById("prod-comprados");
 let contadorProductos = localStorage.getItem("prod-comprados");
 
 function contadorProd() {
-    prodComprados.innerHTML = `${contadorProductos}`;
-    prodComprados.style.display = "block";
-    localStorage.setItem("prod-comprados", contadorProductos);
+  prodComprados.innerHTML = `${contadorProductos}`;
+  prodComprados.style.display = "block";
+  localStorage.setItem("prod-comprados", contadorProductos);
 }
 if (contadorProductos > 0 && contadorProductos != undefined) {
-    contadorProd();
+  contadorProd();
 } else {
-    prodComprados.style.display = "none";
+  prodComprados.style.display = "none";
 }
 
 // ============ FUNCIONALIDAD CARRITO =============
 let idProd = 0;
 const carritoCompleto = JSON.parse(localStorage.getItem("carrito")) || [];
-carritoCompleto.forEach(producto => {
-    let prodCarrito = document.createElement("div");
-    // da ID unico a cada elemento
-    if (carritoCompleto.length > 0) {
-        idProd++;
-    }
-    prodCarrito.innerHTML = `
+carritoCompleto.forEach((producto) => {
+  let prodCarrito = document.createElement("div");
+  // da ID unico a cada elemento
+  if (carritoCompleto.length > 0) {
+    idProd++;
+  }
+  prodCarrito.innerHTML = `
                                 <div class="cart-item">
                                     <p class="card-title">${producto.nombre}</p>
                                     <p class="card-text">$${producto.precio}</p>
                                     <button class="btn btn-danger btn-sm elim-prod" id="borrar${idProd}">x</button>
                                 </div>
-                                `
-    let precio = parseInt(producto.precio);
-    sumaTotal = sumaTotal + precio;
-    carritoContainer.appendChild(prodCarrito);
-    let btnBorrar = document.getElementById(`borrar${idProd}`);
-    btnBorrar.addEventListener('click', (e) => {
-        // modifica navbar
-        sumaTotal = sumaTotal - precio;
-        calcTotal();
-        contadorProductos--;
-        contadorProd();
-        if (contadorProductos <= 0 || contadorProductos == undefined) {
-            prodComprados.style.display = "none";
-            localStorage.setItem("prod-comprados", 0);
-        }
-        borrarProductos(e);
-        // elimina elementos del carrito
-        const index = carritoCompleto.indexOf(producto);
-        if (index > -1) {
-            carritoCompleto.splice(index, 1);
-        }
-        // si el carrito está vacío cambia HTML
-        localStorage.setItem("carrito", JSON.stringify(carritoCompleto));
-        if (carritoCompleto.length == 0) {
-            carritoContainer.innerHTML = `
+                                `;
+  let precio = parseInt(producto.precio);
+  sumaTotal = sumaTotal + precio;
+  carritoContainer.appendChild(prodCarrito);
+  let btnBorrar = document.getElementById(`borrar${idProd}`);
+  btnBorrar.addEventListener("click", (e) => {
+    // modifica navbar
+    sumaTotal = sumaTotal - precio;
+    calcTotal();
+    contadorProductos--;
+    contadorProd();
+    if (contadorProductos <= 0 || contadorProductos == undefined) {
+      prodComprados.style.display = "none";
+      localStorage.setItem("prod-comprados", 0);
+    }
+    borrarProductos(e);
+    // elimina elementos del carrito
+    const index = carritoCompleto.indexOf(producto);
+    if (index > -1) {
+      carritoCompleto.splice(index, 1);
+    }
+    // si el carrito está vacío cambia HTML
+    localStorage.setItem("carrito", JSON.stringify(carritoCompleto));
+    if (carritoCompleto.length == 0) {
+      carritoContainer.innerHTML = `
                                         <h3>El carrito está vacío</h3>
                                         <a class="login-submit btn btn-primary" href="../index.html">¡Compra algo!</a>
-                                        `
-            confirmaCompra.style.display = "none";
-        }
-    })
-})
+                                        `;
+      confirmaCompra.style.display = "none";
+    }
+  });
+});
 
 // función que borra elemento visual del carrito
 function borrarProductos(e) {
-    let botonBorrarClick = e.target;
-    botonBorrarClick.parentElement.remove();
+  let botonBorrarClick = e.target;
+  botonBorrarClick.parentElement.remove();
 }
 
 // ============ CARRITO VACÍO ===========
 if (carritoCompleto === undefined || carritoCompleto.length == 0) {
-    carritoContainer.innerHTML = `
+  carritoContainer.innerHTML = `
                                 <h3>El carrito está vacío</h3>
                                 <a class="login-submit btn btn-primary" href="../index.html">¡Compra algo!</a>
-                                `
-    confirmaCompra.style.display = "none";
+                                `;
+  confirmaCompra.style.display = "none";
 } else {
-    // Botón confirmar compra
-    confirmaCompra.style.display = "block";
-    confirmaCompra.onclick = (e) => {
-        localStorage.setItem("prod-comprados", 0);
-        e.preventDefault();
-        // vacía el carrito y actualiza localstorage
-        while (carritoCompleto.length > 0) {
-            carritoCompleto.pop();
-        }
-        localStorage.setItem("carrito", JSON.stringify(carritoCompleto));
-        // crea alerta que al aceptar recarga
-        Swal.fire(
-            '¡Muchas gracias por su compra!',
-            '¡Que tenga un buen día!',
-            'success'
-        ).then((resultado) => {
-            if (resultado.isConfirmed) {
-                window.location.reload();
-            }
-        })
+  // Botón confirmar compra
+  confirmaCompra.style.display = "block";
+  confirmaCompra.onclick = (e) => {
+    localStorage.setItem("prod-comprados", 0);
+    e.preventDefault();
+    // vacía el carrito y actualiza localstorage
+    while (carritoCompleto.length > 0) {
+      carritoCompleto.pop();
     }
+    localStorage.setItem("carrito", JSON.stringify(carritoCompleto));
+    // crea alerta que al aceptar recarga
+    Swal.fire(
+      "¡Muchas gracias por su compra!",
+      "¡Que tenga un buen día!",
+      "success"
+    ).then((resultado) => {
+      if (resultado.isConfirmed) {
+        window.location.reload();
+      }
+    });
+  };
 }
 // ========TOTAL=======
 function calcTotal() {
-    if (sumaTotal == 0) {
-        totalCarrito.style.display = "none";
-    } else {
-        totalCarrito.innerHTML = `Total $${sumaTotal}`;
-    }
+  if (sumaTotal == 0) {
+    totalCarrito.style.display = "none";
+  } else {
+    totalCarrito.innerHTML = `Total $${sumaTotal}`;
+  }
 }
 calcTotal();
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,90 +5,90 @@ let insertUser = document.getElementById("user-profile");
 let prodComprados = document.getElementById("prod-comprados");
 //-----------USO DE API-----------
 const adminLogin = JSON.parse(localStorage.getItem("adminLogin")) || {
-    admin: false
-}
+  admin: false,
+};
 
 // ----------CONDICIONAL SI ES ADMIN-----------
 if (adminLogin.admin) {
-    navLogin.innerText = "Cerrar Sesión";
-    let vistaVendedor = document.getElementById("vista-vendedor");
-    vistaVendedor.innerHTML = "(vista del vendedor)";
-    formulario.style.display = "none";
-    insertUser.innerHTML = `
+  navLogin.innerText = "Cerrar Sesión";
+  let vistaVendedor = document.getElementById("vista-vendedor");
+  vistaVendedor.innerHTML = "(vista del vendedor)";
+  formulario.style.display = "none";
+  insertUser.innerHTML = `
                 <p id="user-name">Hola, Administrador</p>
                 <img src="./assets/img/default-user-img.png" id="user-pic">
-                `
+                `;
 } else {
-    // si adminLogin.admin == false, muestra usuario
-    // oculta botón de crear producto
-    btnCrear.style.display = "none";
-    const crearUsuario = () => {
-        let nombreUser = JSON.parse(sessionStorage.getItem("nombreUser"));
-        let apellidoUser = JSON.parse(sessionStorage.getItem("apellidoUser"));
-        let fotoUser = JSON.parse(sessionStorage.getItem("fotoUser"));
-        insertUser.innerHTML = `
+  // si adminLogin.admin == false, muestra usuario
+  // oculta botón de crear producto
+  btnCrear.style.display = "none";
+  const crearUsuario = () => {
+    let nombreUser = JSON.parse(sessionStorage.getItem("nombreUser"));
+    let apellidoUser = JSON.parse(sessionStorage.getItem("apellidoUser"));
+    let fotoUser = JSON.parse(sessionStorage.getItem("fotoUser"));
+    insertUser.innerHTML = `
                 <p id="user-name">Hola, ${nombreUser} ${apellidoUser}</p>
                 <img src="${fotoUser}" id="user-pic">
-                `
-    }
-    if (sessionStorage.getItem("nombreUser") != null) {
-        crearUsuario();
-    } else {
-        fetch('https://randomuser.me/api/')
-            .then(response => response.json())
-            .then(resultado => {
-                // manda perfil a sessionStorage
-                let apiNombre = resultado.results[0].name.first;
-                let apiApellido = resultado.results[0].name.last;
-                let apiFoto = resultado.results[0].picture.medium;
-                sessionStorage.setItem("nombreUser", JSON.stringify(apiNombre));
-                sessionStorage.setItem("apellidoUser", JSON.stringify(apiApellido));
-                sessionStorage.setItem("fotoUser", JSON.stringify(apiFoto));
-                // generar html con API
-                insertUser.innerHTML = `
+                `;
+  };
+  if (sessionStorage.getItem("nombreUser") != null) {
+    crearUsuario();
+  } else {
+    fetch("https://randomuser.me/api/")
+      .then((response) => response.json())
+      .then((resultado) => {
+        // manda perfil a sessionStorage
+        let apiNombre = resultado.results[0].name.first;
+        let apiApellido = resultado.results[0].name.last;
+        let apiFoto = resultado.results[0].picture.medium;
+        sessionStorage.setItem("nombreUser", JSON.stringify(apiNombre));
+        sessionStorage.setItem("apellidoUser", JSON.stringify(apiApellido));
+        sessionStorage.setItem("fotoUser", JSON.stringify(apiFoto));
+        // generar html con API
+        insertUser.innerHTML = `
                 <p id="user-name">Hola, ${apiNombre} ${apiApellido}</p>
                 <img src="${apiFoto}" id="user-pic">
-                `
-            })
-            .catch(error => console.log(error), insertUser.innerHTML = `
+                `;
+      })
+      .catch(
+        (error) => console.log(error),
+        (insertUser.innerHTML = `
             <p id="user-name">Cargando usuario...</p>
             <img src="./assets/img/default-user-img.png" id="user-pic">
             `)
-    }
+      );
+  }
 }
-
-
 
 // ---------FORMULARIO---------
 // control de boton que activa formulario
 const estadoFormulario = {
-    mostrar: false
-}
+  mostrar: false,
+};
 
 btnCrear.onclick = (e) => {
-    e.preventDefault();
-    console.log(adminLogin.admin);
-    if (estadoFormulario.mostrar) {
-        btnCrear.innerText = "Crear un producto";
-        formulario.style.display = "none";
-        estadoFormulario.mostrar = false;
-        console.log(estadoFormulario);
-    } else {
-        btnCrear.innerText = "Cancelar";
-        formulario.style.display = "flex";
-        estadoFormulario.mostrar = true;
-        console.log(estadoFormulario);
-    }
-}
-
+  e.preventDefault();
+  console.log(adminLogin.admin);
+  if (estadoFormulario.mostrar) {
+    btnCrear.innerText = "Crear un producto";
+    formulario.style.display = "none";
+    estadoFormulario.mostrar = false;
+    console.log(estadoFormulario);
+  } else {
+    btnCrear.innerText = "Cancelar";
+    formulario.style.display = "flex";
+    estadoFormulario.mostrar = true;
+    console.log(estadoFormulario);
+  }
+};
 
 // control de agregado de productos
 class Producto {
-    constructor(nombre, precio, id) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.id = id;
-    }
+  constructor(nombre, precio, id) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.id = id;
+  }
 }
 
 let crearProd = document.getElementById("btn-crear");
@@ -96,27 +96,27 @@ let crearProd = document.getElementById("btn-crear");
 // lee el localStorage y si está vacío le asigna productos por default
 // operador ternario
 const listaProductos = JSON.parse(localStorage.getItem("productos")) || [
-    { nombre: `SMART TV SAMSUNG SERIES 7 LED 4K 50"`, precio: 80000, id: 1 },
-    { nombre: "NOTEBOOK DELL INSPIRON 3502", precio: 83599, id: 2 },
-    { nombre: "CELULAR SAMSUNG A51 128GB", precio: 64000, id: 3 },
-    { nombre: "MEMORIA RAM FURY BEAST DDR4 8GB", precio: 7300, id: 4 },
+  { nombre: `SMART TV SAMSUNG SERIES 7 LED 4K 50"`, precio: 80000, id: 1 },
+  { nombre: "NOTEBOOK DELL INSPIRON 3502", precio: 83599, id: 2 },
+  { nombre: "CELULAR SAMSUNG A51 128GB", precio: 64000, id: 3 },
+  { nombre: "MEMORIA RAM FURY BEAST DDR4 8GB", precio: 7300, id: 4 },
 ];
 
 // Función para agregar producto al array nuevo con value de inputs
 let id = 4;
 const agregarProducto = () => {
-    let nombre = document.getElementById("nombre").value;
-    nombre = nombre.toUpperCase();
-    let precio = document.getElementById("precio").value;
-    id++;
-    console.log("el id es: " + id);
-    let productoNuevo = new Producto(nombre, precio, id);
-    listaProductos.unshift(productoNuevo);
+  let nombre = document.getElementById("nombre").value;
+  nombre = nombre.toUpperCase();
+  let precio = document.getElementById("precio").value;
+  id++;
+  console.log("el id es: " + id);
+  let productoNuevo = new Producto(nombre, precio, id);
+  listaProductos.unshift(productoNuevo);
 
-    // guarda el producto en el localStorage
-    localStorage.setItem("productos", JSON.stringify(listaProductos));
-    return productoNuevo;
-}
+  // guarda el producto en el localStorage
+  localStorage.setItem("productos", JSON.stringify(listaProductos));
+  return productoNuevo;
+};
 
 // crea array del carrito
 
@@ -126,82 +126,32 @@ let btnAgregar = document.getElementById("agregar");
 let idProd = 0;
 // se crea la card con el contenido del formulario
 btnAgregar.onclick = (e) => {
-    e.preventDefault();
-    if (nombre.value != "" && precio.value != "") {
-        document.getElementById("main-cards").innerHTML = "";
-        agregarProducto();
-        document.getElementById("nombre").value = "";
-        document.getElementById("precio").value = "";
-        listaProductos.forEach(elemento => {
-            idProd++;
-            let nodo = document.createElement("div")
-            nodo.setAttribute("class", "card");
-            nodo.setAttribute("style", "width: 18rem;");
-            nodo.innerHTML = `
+  e.preventDefault();
+  if (nombre.value != "" && precio.value != "") {
+    document.getElementById("main-cards").innerHTML = "";
+    agregarProducto();
+    document.getElementById("nombre").value = "";
+    document.getElementById("precio").value = "";
+    listaProductos.forEach((elemento) => {
+      idProd++;
+      let nodo = document.createElement("div");
+      nodo.setAttribute("class", "card");
+      nodo.setAttribute("style", "width: 18rem;");
+      nodo.innerHTML = `
                 <a class="close-icon"><i class="fa-solid fa-xmark"></i></a>
-                <img src="https://dummyimage.com/600x400/000/fff" class="card-img-top" alt="${elemento.nombre}">ag
+                <img src="https://dummyimage.com/600x400/000/fff" class="card-img-top" alt="${elemento.nombre}">
                 <div class="card-body" id="card-body">
                     <h5 class="card-title">${elemento.nombre}</h5>
                     <p class="card-text">$${elemento.precio}</p>
                     <button class="btn btn-primary" id="button${idProd}">Añadir al carrito</button>
                 </div>
-            `
-            document.getElementById("main-cards").appendChild(nodo);
+            `;
+      document.getElementById("main-cards").appendChild(nodo);
 
-            const addToCart = document.getElementById(`button${idProd}`);
-            console.log(elemento.id);
+      const addToCart = document.getElementById(`button${idProd}`);
+      console.log(elemento.id);
 
-            addToCart.addEventListener('click', () => {
-                // modifica navbar
-                contadorProductos++;
-                contadorProd();
-                // Agrega al array carrito el producto y lo sube a localStorage
-                carrito.unshift(elemento);
-                localStorage.setItem("carrito", JSON.stringify(carrito));
-
-                Swal.fire("Agregaste: " + elemento.nombre);
-                console.log(`clickeado ${elemento.id}`);
-            })
-        })
-    } else {
-        Swal.fire("Por favor, ingrese un valor en ambos campos");
-    }
-}
-
-// ------NUMERO CARRITO EN NAVBAR----------
-let contadorProductos = localStorage.getItem("prod-comprados");
-
-function contadorProd() {
-    prodComprados.innerHTML = `${contadorProductos}`;
-    prodComprados.style.display = "block";
-    localStorage.setItem("prod-comprados", contadorProductos);
-}
-if (contadorProductos > 0 && contadorProductos != undefined) {
-    contadorProd();
-} else {
-    prodComprados.style.display = "none";
-}
-
-// Crea las cards de los productos del array ya declarados desde el inicio
-listaProductos.forEach(elemento => {
-    idProd++;
-    let nodo = document.createElement("div")
-    nodo.setAttribute("class", "card");
-    nodo.setAttribute("style", "width: 18rem;");
-    nodo.innerHTML = `
-        <a class="close-icon"><i class="fa-solid fa-xmark"></i></a>
-        <img src="https://dummyimage.com/600x400/000/fff" class="card-img-top" alt="${elemento.nombre}">
-        <div class="card-body" id="card-body">
-            <h5 class="card-title">${elemento.nombre}</h5>
-            <p class="card-text">$${elemento.precio}</p>
-            <button class="btn btn-primary" id="button${idProd}">Añadir al carrito</button>
-        </div> 
-        `
-    document.getElementById("main-cards").appendChild(nodo);
-    const addToCart = document.getElementById(`button${idProd}`);
-    console.log(elemento.id);
-
-    addToCart.addEventListener('click', () => {
+      addToCart.addEventListener("click", () => {
         // modifica navbar
         contadorProductos++;
         contadorProd();
@@ -211,5 +161,55 @@ listaProductos.forEach(elemento => {
 
         Swal.fire("Agregaste: " + elemento.nombre);
         console.log(`clickeado ${elemento.id}`);
-    })
-})
+      });
+    });
+  } else {
+    Swal.fire("Por favor, ingrese un valor en ambos campos");
+  }
+};
+
+// ------NUMERO CARRITO EN NAVBAR----------
+let contadorProductos = localStorage.getItem("prod-comprados");
+
+function contadorProd() {
+  prodComprados.innerHTML = `${contadorProductos}`;
+  prodComprados.style.display = "block";
+  localStorage.setItem("prod-comprados", contadorProductos);
+}
+if (contadorProductos > 0 && contadorProductos != undefined) {
+  contadorProd();
+} else {
+  prodComprados.style.display = "none";
+}
+
+// Crea las cards de los productos del array ya declarados desde el inicio
+listaProductos.forEach((elemento) => {
+  idProd++;
+  let nodo = document.createElement("div");
+  nodo.setAttribute("class", "card");
+  nodo.setAttribute("style", "width: 18rem;");
+  nodo.innerHTML = `
+        <a class="close-icon"><i class="fa-solid fa-xmark"></i></a>
+        <img src="https://dummyimage.com/600x400/000/fff" class="card-img-top" alt="${elemento.nombre}">
+        <div class="card-body" id="card-body">
+            <h5 class="card-title">${elemento.nombre}</h5>
+            <p class="card-text">$${elemento.precio}</p>
+            <button class="btn btn-primary" id="button${idProd}">Añadir al carrito</button>
+        </div> 
+        `;
+  document.getElementById("main-cards").appendChild(nodo);
+  const addToCart = document.getElementById(`button${idProd}`);
+  console.log(elemento.id);
+
+  addToCart.addEventListener("click", () => {
+    // modifica navbar
+    contadorProductos++;
+    contadorProd();
+    // Agrega al array carrito el producto y lo sube a localStorage
+    carrito.unshift(elemento);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+
+    Swal.fire("Agregaste: " + elemento.nombre);
+    console.log(`clickeado ${elemento.id}`);
+  });
+});
